9f76a48d93fb7bd70139684e1837a748fc5035d7
Mykola Hubytskyi <m.hubytskyi@gmail.com>
2022-05-30 09:45:09 +0300

    changed PaperWrapper path

--- src/components/corporate-insurance/list/index.jsx
+++ src/components/corporate-insurance/list/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Container } from '@mui/material';
-import PaperWrapper from '../../common/paper';
+import PaperWrapper from '../../Common/paper';
 import styles from './styles';
 import { graphql, Link, useStaticQuery } from 'gatsby';
 
