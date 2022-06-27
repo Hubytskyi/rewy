6131e3b7042ab6cda649acbf20cd16ad067b965f
Mykola Hubytskyi <m.hubytskyi@gmail.com>
2022-06-27 22:25:44 +0300

    add check for pdf page

--- src/pages/pdf.js
+++ src/pages/pdf.js
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { PDFViewer } from '@react-pdf/renderer';
-import PDF from "../components/PDF"
+import PDF from '../components/PDF';
 
-const PDFPage = () => (
-  <PDFViewer style={{height: '100vh', width: '100vw'}}>
-    <PDF />
-  </PDFViewer>
-);
+const PDFPage = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
+
+  return (
+    <div>
+      {isClient && (
+        <PDFViewer style={{height: '100vh', width: '100vw'}}>
+          <PDF/>
+        </PDFViewer>
+      )}
+    </div>
+
+  );
+};
 
 export default PDFPage;
